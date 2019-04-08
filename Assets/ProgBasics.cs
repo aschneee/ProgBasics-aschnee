23c9888fb307915cebe6e2773568ac9afd9e9402
aschnee <alina.schneeberger@htl-salzburg.ac.at>
2019-04-08 15:43:15 +0200

    created script and initialized debug.log(erste Tests)

---
+++ Assets/ProgBasics.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgBasics : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("erste Tests mit Operatoren");
+
+   
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
