18802ac36762da5171a556ceef31315be2c3ffaf
Alina <48963514+aschneee@users.noreply.github.com>
2019-04-09 09:18:09 +0200

    ProgBasics, updated style stucture

--- Assets/MyGame/Scripts/ProgBasics.cs
+++ Assets/MyGame/Scripts/ProgBasics.cs
@@ -6,37 +6,37 @@ public class ProgBasics : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Debug.Log("erste Tests mit Operatoren");
+             Debug.Log("erste Tests mit Operatoren");
 
-     //Multiplikative Operatoren
-     Debug.Log("Multiplikative Operatoren: 2*3 = " + (2 * 3));
-     Debug.Log("Multiplikative Operatoren: 100/3 = " + (100 / 3));
-     Debug.Log("Multiplikative Operatoren: 20%10 = " + (20 % 10));
-
-
-
-     //Additive Operatoren
-     Debug.Log("Additive Operatoren: 20+4 = " + (20 + 4));
-     Debug.Log("Additive Operatoren: 20-4 = " + (20 - 4));
+	     //Multiplikative Operatoren
+	     Debug.Log("Multiplikative Operatoren: 2*3 = " + (2 * 3));
+	     Debug.Log("Multiplikative Operatoren: 100/3 = " + (100 / 3));
+	     Debug.Log("Multiplikative Operatoren: 20%10 = " + (20 % 10));
 
 
 
-     //Relationale Operatoren
-     Debug.Log("Relationale Operatoren: 20<4 = " + (20 < 4));
-     Debug.Log("Relationale Operatoren: 20<=20 = " + (20 <= 20));
-     Debug.Log("Relationale Operatoren: 20>4 = " + (20 > 4));
-     Debug.Log("Relationale Operatoren: 20>=4 = " + (20 >= 21));
+	     //Additive Operatoren
+	     Debug.Log("Additive Operatoren: 20+4 = " + (20 + 4));
+	     Debug.Log("Additive Operatoren: 20-4 = " + (20 - 4));
 
 
-     //Gleichheits Operatoren
-     Debug.Log("Gleichheits Operatoren: 20==20 = " + (20 == 20));
-     Debug.Log("Gleichheits Operatoren: 10!=20 = " + (10 != 20));
 
-     //Logische Operatoren
-     Debug.Log("Logische Operatoren: true&true = " + (true & true));
-     Debug.Log("Logische Operatoren: false&&true = " + (false && true));
-     Debug.Log("Logische Operatoren: false|true = " + (false | true));
-     Debug.Log("Logische Operatoren: true||false = " + (true || false));
+	     //Relationale Operatoren
+	     Debug.Log("Relationale Operatoren: 20<4 = " + (20 < 4));
+	     Debug.Log("Relationale Operatoren: 20<=20 = " + (20 <= 20));
+	     Debug.Log("Relationale Operatoren: 20>4 = " + (20 > 4));
+	     Debug.Log("Relationale Operatoren: 20>=4 = " + (20 >= 21));
+
+
+	     //Gleichheits Operatoren
+	     Debug.Log("Gleichheits Operatoren: 20==20 = " + (20 == 20));
+	     Debug.Log("Gleichheits Operatoren: 10!=20 = " + (10 != 20));
+
+	     //Logische Operatoren
+	     Debug.Log("Logische Operatoren: true&true = " + (true & true));
+	     Debug.Log("Logische Operatoren: false&&true = " + (false && true));
+	     Debug.Log("Logische Operatoren: false|true = " + (false | true));
+	     Debug.Log("Logische Operatoren: true||false = " + (true || false));
     }
 
 
