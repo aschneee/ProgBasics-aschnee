c47a20cbebd363696be766d61b83dc713c32b7e3
aschnee <alina.schneeberger@htl-salzburg.ac.at>
2019-04-10 13:36:09 +0200

    2 public methods added with Debug Log message

--- Assets/MyGame/Scripts/ProgBasics.cs
+++ Assets/MyGame/Scripts/ProgBasics.cs
@@ -2,43 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProgBasics : MonoBehaviour {
+public class ProgBasics : MonoBehaviour
+{
 
 	// Use this for initialization
 	void Start () {
-             Debug.Log("erste Tests mit Operatoren");
-
-	     //Multiplikative Operatoren
-	     Debug.Log("Multiplikative Operatoren: 2*3 = " + (2 * 3));
-	     Debug.Log("Multiplikative Operatoren: 100/3 = " + (100 / 3));
-	     Debug.Log("Multiplikative Operatoren: 20%10 = " + (20 % 10));
-
-
-
-	     //Additive Operatoren
-	     Debug.Log("Additive Operatoren: 20+4 = " + (20 + 4));
-	     Debug.Log("Additive Operatoren: 20-4 = " + (20 - 4));
-
-
-
-	     //Relationale Operatoren
-	     Debug.Log("Relationale Operatoren: 20<4 = " + (20 < 4));
-	     Debug.Log("Relationale Operatoren: 20<=20 = " + (20 <= 20));
-	     Debug.Log("Relationale Operatoren: 20>4 = " + (20 > 4));
-	     Debug.Log("Relationale Operatoren: 20>=4 = " + (20 >= 21));
-
-
-	     //Gleichheits Operatoren
-	     Debug.Log("Gleichheits Operatoren: 20==20 = " + (20 == 20));
-	     Debug.Log("Gleichheits Operatoren: 10!=20 = " + (10 != 20));
-
-	     //Logische Operatoren
-	     Debug.Log("Logische Operatoren: true&true = " + (true & true));
-	     Debug.Log("Logische Operatoren: false&&true = " + (false && true));
-	     Debug.Log("Logische Operatoren: false|true = " + (false | true));
-	     Debug.Log("Logische Operatoren: true||false = " + (true || false));
+        //aufrufen der public void operators
+        //Klammern sind die Parameter Liste
+        Operators();   
     }
 
+    public void Operators()
+    {
+        Debug.Log("erste Tests mit Operatoren");
+
+        //Multiplikative Operatoren
+        Debug.Log("Multiplikative Operatoren: 2*3 = " + (2 * 3));
+        Debug.Log("Multiplikative Operatoren: 100/3 = " + (100 / 3));
+        Debug.Log("Multiplikative Operatoren: 20%10 = " + (20 % 10));
+
+
+
+        //Additive Operatoren
+        Debug.Log("Additive Operatoren: 20+4 = " + (20 + 4));
+        Debug.Log("Additive Operatoren: 20-4 = " + (20 - 4));
+
+
+
+        //Relationale Operatoren
+        Debug.Log("Relationale Operatoren: 20<4 = " + (20 < 4));
+        Debug.Log("Relationale Operatoren: 20<=20 = " + (20 <= 20));
+        Debug.Log("Relationale Operatoren: 20>4 = " + (20 > 4));
+        Debug.Log("Relationale Operatoren: 20>=4 = " + (20 >= 21));
+
+
+        //Gleichheits Operatoren
+        Debug.Log("Gleichheits Operatoren: 20==20 = " + (20 == 20));
+        Debug.Log("Gleichheits Operatoren: 10!=20 = " + (10 != 20));
+
+        //Logische Operatoren
+        Debug.Log("Logische Operatoren: true&true = " + (true & true));
+        Debug.Log("Logische Operatoren: false&&true = " + (false && true));
+        Debug.Log("Logische Operatoren: false|true = " + (false | true));
+        Debug.Log("Logische Operatoren: true||false = " + (true || false));
+    }
+
+    // Methoden Deklaration
+    public void xy ()
+    {
+
+    }
+
+    public void Test ()
+    {
+        Debug.Log("Hallo du!");
+    }
+
+    public void Test2 ()
+    {
+        Debug.Log("Test2 Logischer Operator: true und true = " + (true & true));
+    }
 
     // Update is called once per frame
     void Update () {
